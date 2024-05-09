a13153c3ca58beafdbb4ae8765fd29fe01499a23
Filmos <filmosyt@gmail.com>
2024-05-09 14:21:38 +0200

    Added two-model emotion mapping aproach for feature based models (not run)

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Speech Emotion Recognition for Autistic Children Therapy
 # Data
 
 Link to raw data (`./dataset`): https://drive.google.com/file/d/1GnyD8SwyARGlTpWxC8w99H3cgcDAlEyu/view?usp=drive_link  
-Link to processed data (`./dataset_processed`): https://wutwaw-my.sharepoint.com/:u:/g/personal/01151411_pw_edu_pl/EUUvmcvZg-9Bmr4bMaXeITkBJ8vbEWriWc2NOgMBfkjLgw?e=UAYFdw
+Link to processed data (`./dataset_processed`): https://zenodo.org/records/10927644
---
+++ notebooks/reusable/emotion_mapping.py
@@ -0,0 +1,64 @@
+from enum import Enum
+
+import numpy as np
+from sklearn.base import clone
+
+
+class EmotionMap(Enum):
+    """
+        Mapping of emotions into two components, valence and arousal.
+        Based on an approximation from https://www.researchgate.net/figure/Emotion-Mapping-in-Valence-Arousal-Domains_fig2_348990229
+    """
+    Anger = (-0.75, 0.25)
+    Disgust = (-0.25, -0.5)
+    Fear = (-0.25, 0.75)
+    Happy = (1, 0.25)
+    Neutral = (0, 0)
+    Sad = (-1, -0.5)
+    Surprise = (0.75, 0.5)
+
+    def map(labels, component: int, *, discrete: bool):
+        continuous = np.array([getattr(EmotionMap, x).value[component] for x in labels])
+        if not discrete:
+            return continuous
+        return np.array(continuous*4+4, dtype=np.int32)
+    
+    def reverse_map(valence, arousal, *, discrete: bool):
+        point = np.array([valence, arousal])
+        if discrete:
+            point = (point-4)/4
+        enum_values = np.array([emotion.value for emotion in EmotionMap])
+        distances = np.linalg.norm(enum_values - point, axis=1)
+        min_index = np.argmin(distances)
+        closest_emotion = np.array([emotion.name for emotion in EmotionMap])[min_index]
+    
+        return closest_emotion
+
+class TwoModelWrapper:
+    def __init__(self, valenceModel, arousalModel):
+        self.valenceModel = valenceModel
+        self.arousalModel = arousalModel
+        self._discrete = False
+
+    def discrete(self):
+        self._discrete = True
+        return self
+
+    def fit(self, X_train, y_train):
+        self.valenceModel.fit(X_train, EmotionMap.map(y_train, 0, discrete=self._discrete))
+        self.arousalModel.fit(X_train, EmotionMap.map(y_train, 1, discrete=self._discrete))
+        return self
+    
+    def predict(self, X_test):
+        valence = self.valenceModel.predict(X_test)
+        arousal = self.arousalModel.predict(X_test)
+        return np.array([EmotionMap.reverse_map(valence[i], arousal[i], discrete=self._discrete) for i in range(len(valence))])
+
+    @property
+    def best_params_(self):
+        return [self.valenceModel.best_params_, self.arousalModel.best_params_]
+
+
+class SklearnTwoModelWrapper(TwoModelWrapper):
+    def __init__(self, baseModel):
+        super().__init__(clone(baseModel), clone(baseModel))
